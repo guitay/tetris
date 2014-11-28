4a9e73327cf143be5405d0655b4f5c4ddea5a073
guitay <nongfei@gmail.com>
2014-11-28 15:10:46 +0800

    first

---
+++ tetris.py
@@ -0,0 +1,24 @@
+# -*- coding: cp936 -*-
+
+class Shape():
+    """Õº–Õ¿‡ """
+    def __init__(self):
+        pass
+
+    def down(self):
+        pass
+
+    def move(self,direction):
+        pass
+
+    def rotate(self):
+        pass
+
+    
+    
+class Board():
+    def __init__(self):
+        self.width = 10
+        self.height = 20
+
+    def 
